6d46f764144acc0f79c35961cd17b5ec7ef34177
JStone1 <62293789+JStone1@users.noreply.github.com>
2023-03-21 12:20:40 +0000

    Add in functions to add user and receive list of users

--- index.js
+++ index.js
@@ -3,5 +3,28 @@ const app = express();
 app.listen(3000, () => console.log("Listening at port 3000"));
 
 app.use(express.static("public"));
-
 app.use(express.json());
+
+const path = require("path");
+
+let userData = [
+  { name: "John", age: 23 },
+  { name: "Anna", age: 24 },
+];
+
+app.get("/data", (request, response) => {
+  response.json({
+    time: Date.now(),
+    data: userData,
+  });
+});
+
+app.post("/newUser", (request, response) => {
+  console.log("Request.body = ", request.body);
+  let newUser = request.body;
+  userData.push(newUser);
+  response.json({
+    status: "successful",
+    numberOfUsers: userData.length,
+  });
+});
