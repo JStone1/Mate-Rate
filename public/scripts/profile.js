791acc96b99af28c0db59d6dddcccaa2f15357ea
JStone1 <62293789+JStone1@users.noreply.github.com>
2023-05-31 17:46:07 +0100

    Applied initial colour scheme

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -24,10 +24,18 @@ let newBioText = document.getElementById("bio-text");
 console.log(newBioText);
 let bioBtn = document.getElementById("update-bio-btn");
 
+newBioText.addEventListener("click", () => {
+  document.getElementsByClassName("update-success")[0].classList.add("hidden");
+});
+
 bioBtn.addEventListener("click", async () => {
+  document
+    .getElementsByClassName("update-success")[0]
+    .classList.remove("hidden");
   let userInfo = {
     bio: newBioText.value,
   };
+  newBioText.value = "";
   fetch("/updateBio", {
     method: "POST",
     headers: {
@@ -41,7 +49,14 @@ let passwordBtn = document.getElementById("update-password-btn");
 let newPassword = document.getElementById("new-password-text");
 console.log(newPassword);
 
+newPassword.addEventListener("click", () => {
+  document.getElementsByClassName("update-success")[1].classList.add("hidden");
+});
+
 passwordBtn.addEventListener("click", async () => {
+  document
+    .getElementsByClassName("update-success")[1]
+    .classList.remove("hidden");
   let userInfo = {
     password: newPassword.value,
   };
